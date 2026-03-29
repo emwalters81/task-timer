e8fd377c201b111890b8262e0f08ac7e0ef85f49
Isaac Lanning <il225023@ohio.edu>
2026-03-29 19:51:00 -0400

    need to install vitest soon
    this was just for the test folder and example file

---
+++ tests/timer_tests.svelte.ts
@@ -0,0 +1,10 @@
+import { flushSync } from "svelte";
+import { expect, test } from "vitest";
+import  Timer from "$lib/Timer.svelte";
+
+
+// i plan to use the timer component i made to unit test. im sure that vitest is
+// actually installed yet, and i feel that i would definitely break something if
+// i tried to install it. i plan to update this file so please make your own for
+// the actual tests. also heres the docs: https://svelte.dev/docs/svelte/testing
+// - isaac
