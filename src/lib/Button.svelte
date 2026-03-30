8b67210beff548fa8d4da2104bc1df27bba2e845
Elijah Flader <elijahflader@gmail.com>
2026-03-29 22:03:25 -0400

    Edit Button

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,9 +1,11 @@
 <script>
     var {text, color="bg-white", onClick} = $props()
+
+    var onclick = $state(false)
 </script>
 
 
-<div class={"pr-10 pl-10 pb-2 pt-2 shadow-[4px_4px_0px_0px_#000] m-2 hover:bg-gray-300 border-2 cursor-pointer " +
+<div class={"pr-10 pl-10 pb-2 pt-2 shadow-[4px_4px_0px_0px_#000] m-2 border-2 hover:shadow-[6px_6px_0px_0px_#000] hover:-translate-x-0.5 hover:-translate-y-0.5 cursor-pointer " +
 "border-black rounded-2xl flex justify-start items-start " + color}>
     {text}
 </div>
