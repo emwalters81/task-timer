ec3f33e32465dbf34f9c8bb6fa5d900202ce7ed2
Elijah Flader <elijahflader@gmail.com>
2026-03-31 14:14:25 -0400

    Fix the broken webpage

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	// ...
+	plugins: [tailwindcss(), sveltekit()],
 	// Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
 	resolve: process.env.VITEST
 		? {
